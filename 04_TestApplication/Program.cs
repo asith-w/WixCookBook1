f5bc680091715f313caafade48e7a2b148d43893
Mykhailo.Markov <Mykhailo.Markov@ODS1-LHP-F49120>
2016-07-29 15:01:54 +0300

    Add project files.

---
+++ 04_TestApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("04 Console Test App");
+            Console.ReadKey();
+        }
+    }
+}
