e9c18cb743f7e361fdac1fe755cad8ebb5b4cc10
Mike <michaelmarkov85@gmail.com>
2016-07-29 21:16:00 +0300

    Chapter 3

---
+++ 09_ConsoleApp64Bits/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_ConsoleApp64Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.Is64BitProcess)
+            {
+                Console.WriteLine("Process is 64-bits!");
+            }
+            else
+            {
+                Console.WriteLine("Process is NOT 64-bits!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp64Bits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp64Bits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.Is64BitProcess)
+            {
+                Console.WriteLine("Process is 64-bits!");
+            }
+            else
+            {
+                Console.WriteLine("Process is NOT 64-bits!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
