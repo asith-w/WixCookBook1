8797e65d955da37b32947aaca98fb6ef0c5c26a0
Mike <michaelmarkov85@gmail.com>
2016-07-29 22:26:44 +0300

    Chapter 4

---
+++ 16_XyzHandler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+namespace XyzHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || string.IsNullOrEmpty(args[0]))
+            {
+                Console.WriteLine("No file name given.");
+            }
+            else
+            {
+                string fileName = args[0];
+                if (File.Exists(fileName))
+                {
+                    Console.WriteLine(File.ReadAllText(fileName));
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
